75a86dc98058f3816df890f7ca807bcdff346931
alex123olula <64591648+alex123olula@users.noreply.github.com>
2020-09-16 04:47:16 +0300

    generated deployables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
+
 export const environment = {
-  production: true
+  production: true,
+  configFirebase: {
+    apiKey           : 'AIzaSyDJiClaLsdwGOPkrMDLD55c_YavoGHHRUw',
+    authDomain       : 'homework-5-e962d.firebaseapp.com',
+    databaseURL      : 'https://homework-5-e962d.firebaseio.com',
+    projectId        : 'homework-5-e962d',
+    storageBucket    : 'homework-5-e962d.appspot.com',
+    messagingSenderId: '127909762244',
+    appId            : '1:127909762244:web:e6a8753acdfef4c30d6591',
+    measurementId    : 'G-F53ECEZ25T'
+  },
+  baseUrl       : 'https://pokeapi.co/api/v2'
 };
